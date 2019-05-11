2dee247ff16496791d38058d84fa57cfc48295a1
N Ouyang <nouyang@alum.mit.edu>
2019-05-11 05:23:47 -0400

    cartpole works! checked against ECE680 notes

--- Simulation/extended_reactionwheel/sym_inertialpend.py
+++ Simulation/extended_reactionwheel/sym_inertialpend.py
@@ -23,17 +23,17 @@ import time
 # we only have theta for the simple pendulum
 # the force input -- there is none
 #q, qdot, qddot = syms('q qdot qddot')
-x, theta, xdot, tdot, xddot, tddot, u = syms('x theta xdot tdot xddot tddot u')
+t1, t2, t1dot, t2dot, t1ddot, t2ddot, tau = syms('t1 t2 t1dot t2dot t1ddot t2ddot tau')
 
 # parameters
 #m, l, g = syms('m l g')
-M, m, g, len = syms('M m g len')
+m1, l1, I1, m2, l2, I2, g = syms('m1 l1 I1 m2 l2 I2 g')
 
-p = Matrix([M, m, g, len])      # parameter vector
-q = Matrix([x, theta])
+p = Matrix([m1, l1, I1, m2, l2, I2, g])      # parameter vector
+q = Matrix([t1, t2])
 
-qdot = Matrix([xdot, tdot])  # time derivative of q
-qddot = Matrix([xddot, tddot])  # time derivative of qdot
+qdot = Matrix([t1dot, t2dot]) # time derivative of q
+qddot = Matrix([t1ddot, t2ddot]) # time derivative of qdot
 
 #qdot = diff(q, t)
 #qddot = diff(qdot, t)
@@ -57,21 +57,16 @@ qddot = Matrix([xddot, tddot])  # time derivative of qdot
     # 0.5 * m1 * v_pend.T * v_pend 
 
 
-K = Matrix([0.5 * (M + m) * xdot**2 + \
-    m * len * xdot * tdot * cos(theta) + \
-    0.5 * m * len**2 * tdot**2])
-
-# K_translat = Matrix([0.5 * m1 * (l1 * t1dot)**2 + \
-    # 0.5 * m2 * (l2 * t2dot)**2])
+K_translat = Matrix([0.5 * m1 * (l1 * t1dot)**2 + \
+    0.5 * m2 * (l2 * t2dot)**2])
 # K_inertial = 0.5 * Matrix([m1 * (l1 * t1dot)**2, 
                            # m2 * (l2 * t2dot)**2])
 
-# K_inertial = Matrix([0.5 * I1 * t1dot**2 + \
-    # 0.5 * I2 * t1dot**2 + \
-    # 0.5 * I2 * t2dot**2])
+K_inertial = Matrix([0.5 * I1 * t1dot**2 + \
+    0.5 * I2 * t1dot**2 + \
+    0.5 * I2 * t2dot**2])
 
-# P = Matrix([m1 * g * (1 - l1 * cos(t1)) + m2 * g * (1 - l2 * cos(t1))])
-P = Matrix([-1 * m * g * len * (1 - cos(theta))])
+P = Matrix([m1 * g * (1 - l1 * cos(t1)) + m2 * g * (1 - l2 * cos(t1))])
 
 # P = Matrix([m1 * g * (1 - l1 * cos(t1)), 
             # m2 * g * (1 - l2 * cos(t1))])
@@ -85,8 +80,7 @@ P = Matrix([-1 * m * g * len * (1 - cos(theta))])
 # K = 0.5 * m * l**2 * (qdot.T * qdot) 
 
 # Lagrangian L=sum(K)-sum(P)
-# L =  K_translat + K_inertial - P
-L =  K - P
+L =  K_translat + K_inertial - P
 
 print(L)
 print('L', L.shape)
@@ -94,23 +88,23 @@ print('q', q.shape)
 
 # first term in the Euler-Lagrange equation
 partial_L_by_partial_q = L.jacobian(Matrix([q])).T
-print('\ndL dq', partial_L_by_partial_q)
 print('dL dq', partial_L_by_partial_q.shape)
+print('dL dq', partial_L_by_partial_q)
 
+print('qdot', qdot)
 # inner term of the second part of the Euler-Lagrange equation
 partial_L_by_partial_qdot = L.jacobian(Matrix([qdot]))
-print('\ndL dqdot', partial_L_by_partial_qdot)
+print(partial_L_by_partial_qdot)
 print('dL dqdot', partial_L_by_partial_qdot.shape)
 
 # second term (overall, time derivative) in the Euler-Lagrange equation
 # applies the chain rule
 d_inner_by_dt = partial_L_by_partial_qdot.jacobian(Matrix([q])) * qdot + \
     partial_L_by_partial_qdot.jacobian(Matrix([qdot])) * qddot
-print('\nd/dt dL/d_qdot', d_inner_by_dt)
+
 
 # Euler-Lagrange equation
-lagrange_eq = partial_L_by_partial_q - d_inner_by_dt + Matrix([0, 0])
-print('\nlagrange eq', lagrange_eq)
+lagrange_eq = partial_L_by_partial_q - d_inner_by_dt + Matrix([0, tau])
 
 # solve the lagrange equation for qddot and simplify
 prevTime = time.time()
@@ -122,18 +116,17 @@ prevTime = time.time()
 
 print("Simplifying...")
 #qddot = simplify(r)
-xddot = simplify(r[xddot])
-tddot = simplify(r[tddot])
+t1ddot = simplify(r[t1ddot])
+t2ddot = simplify(r[t2ddot])
 
-print('xddot= {}\n'.format(xddot));
-print('tddot= {}\n'.format(tddot));
+print('t1ddot= {}\n'.format(t1ddot));
+print('t2ddot= {}\n'.format(t2ddot));
 
 print('Elapsed: ', time.time() - prevTime)
 prevTime = time.time()
 # generate python function
-print(x, theta, xdot, tdot, xddot, tddot, u)
-print(M, m, g, len)
-
+print(t1, t2, t1dot, t2dot, t1ddot, t2ddot)
+print(m1, l1, I1, m2, l2, I2, g)
 
 f_gen = open("reaction_pendulum_dynamics_generated.py", 'w')
 
