bd42103d46934cd5a3706f91e430486e76a4e6cd
N Ouyang <nouyang@alum.mit.edu>
2019-05-13 02:41:46 -0400

    combined_PD.ino.bkup2

--- sanity/combined_PD/combined_PD.ino
+++ sanity/combined_PD/combined_PD.ino
@@ -19,14 +19,16 @@ int n = LOW;
 /*float CVal;    // adjusted Amps value*/
 
 // --------Motor--------
-/*int EnablePin = 8;*/
-/*int duty;*/
-/*int PWMPin = 11;  // Timer2*/
-/*int PWMPin2 = 10;*/
+int EnablePin = 8;
+int duty;
+int PWMPin = 11;  // Timer2
+int PWMPin2 = 10;
 MegaMotoHB motor(11, 10, 8);
 int motor_output = 0; // command to motor
 
 // --------P-Controller--------
+int sample_time = 0; // 15 msec
+
 double theta1 = 0.0; // get_from_encoder()
 double theta2 = 0.0; // get_from_encoder()
 double prev_theta1 = 0.0; // get_from_encoder()
@@ -38,22 +40,29 @@ double theta2dot = 0.0; // get_from_encoder()
 double delta_theta2 = 0.0; // get_from_encoder()
 
 double theta1_desired = 0.0;
+double theta1dot_desired = 0.0;
+
+double err_theta = 0.0;
+double err_thetadot = 0.0;
+
+int delta_motor = 0;
+int prev_motor = 0;
 
 bool theta_CW;
 bool motor_CW;
 
-unsigned long curr_time;
+unsigned long now = 0;
 unsigned long time_elapsed;
 unsigned long prev_time = 0;
 
 double state[4];
 
-double k = 2; // P constant
-
+double k = 4; // theta constant 
+double kdot = -1000; // thetadot 
 
 void setup() {
     /*Serial.begin(230400);*/
-    Serial.begin(9600); // for use with plotter tool 
+    Serial.begin(115200); // for use with plotter tool 
 
     rMotor.begin();
     rStick.begin();
@@ -63,16 +72,17 @@ void setup() {
     PCMSK1 |= (1 << PCINT13) | (1 << PCINT12);
     sei();
 
+    /*motorOn();*/
     motor.Enable();
     motor.SetStepDelay(0);
 }
 
 void loop(){
     // -------- update time --------
-
-    curr_time = millis();
-    time_elapsed = curr_time - prev_time;
-    prev_time = curr_time;
+    unsigned long now = millis();
+    time_elapsed = (now - prev_time);
+    if (time_elapsed >= sample_time)
+    {
 
     // -------- update theta --------
     // issue: prev_theta is the same as theta
@@ -90,104 +100,90 @@ void loop(){
     state[2] = theta1dot;
     state[3] = theta2dot;
 
-//    if (theta1dot > 0) {
-//        theta_CW = true;
-//    }
-//    else if (theta1dot < 0){
-//        theta_CW = false;
-//    }
-    if (delta_theta1 < 0) {
-      theta_CW = true;
-    }
-    else {
-      theta_CW = false;
-    }
-
     // -------- determine motor input --------
-    /*thetadot = delta_theta / time_elapsed;*/
-    /*motor_speed = k * (thetadot - thetadot_desired);*/
     /*Serial.println(motor_speed);*/
     
-    /*motor_output = - ceil(k * (theta1 - theta1_desired));*/
-    motor_output = - 30;
-//    Serial.print("Motor output: ");
+    err_theta = theta1 - theta1_desired; 
+    err_thetadot = theta1dot - theta1dot_desired;
+    motor_output = - ceil( k * (theta1 - theta1_desired) - kdot * (theta1dot - theta1dot_desired));
+    delta_motor = motor_output - prev_motor;
+    prev_motor = motor_output;
 //    Serial.print(motor_output);
     /*aprintf("\ntheta1 %f, t2 %f, t1dot %f, t2dot %f, out %d, deltath %f, cw ", */
             /*theta1, theta2, theta1dot, theta2dot, motor_output, delta_theta1);*/
-    /*Serial.println(motor_output*10);*/
-    Serial.print(theta2);
-    Serial.print(" ");
-    Serial.print(motor_output);
-    Serial.print(" ");
-    Serial.println(theta1);
-//    Serial.println();/*}*/
+    /*aprintf("\n %d %f %d %f %f ", delta_motor, theta1, motor_output, err_theta, err_thetadot);*/
+    /*aprintf("\n %f %f %f %f ", theta1, err_theta, theta1dot, err_thetadot);*/
+    /*aprintf("\n %f %f ", theta1dot, err_thetadot);*/
+    aprintf("\n %f %f %d", theta1, err_theta, motor_output);
+    /*Serial.println(theta1);*/
+    /*Serial.print(delta_motor);*/
     
-//    delay(100);
     // -------- write appropriate motor input --------
-    motor_output  = abs(constrain(motor_output, -255, 255));
+    motor_output  = constrain(motor_output, -255, 255);
+    
+    if (abs(theta1) > 10){
+        if (motor_output > 0){
+            motor.Fwd(abs(motor_output));
+        }
+        else {
+            motor.Rev(abs(motor_output));
+        }
+    }
+    else {
+        Serial.println("stop");
+        motor.Stop();
+    }
+/*
+    motor.Fwd(200);
+    Serial.println("Fwd");
+    delay(500);
+    motor.Rev(200);
+    Serial.println("Rev");
+    delay(500);
+    motor.Stop();
+    Serial.println("Stop");
+    delay(1000);
+    */
+
+
+    /*motorCounterCW(200); */
+    /*motor.Rev(100); // keeps going */
+    /*delay(500);*/
+    /*motor.Stop();*/
+    /*delay(500);*/
 //    if (abs(theta1) < 2.0) {
-//        
 /*//    }*/
-    if (theta1 > 0) { //to the left 
-        if (theta_CW == true) {
-            motor.Fwd(motor_output);
-            /*Serial.print("running CCW");*/
-            /*Serial.print(abs(motor_output));*/
-        }
-        else {
-            motor.Rev(motor_output);
-            /*motorCW(abs(motor_output));*/
-            /*Serial.print("running CW");*/
-            /*Serial.print(abs(motor_output));*/
-        }
+    prev_time = now;
     }
-    else if (theta1 < 0) {
-        if (theta_CW == true) {
-            motor.Rev(motor_output);
-            /*motorCCW(abs(motor_output));*/
-            /*Serial.print("running CCW");*/
-            /*Serial.print(abs(motor_output));*/
-        }
-        else {
-            motor.Fwd(motor_output);
-            /*motorCW(abs(motor_output));*/
-            /*Serial.print("running CW");*/
-            /*Serial.print(abs(motor_output));*/
-        }
-    }
-
-
-
-
 }
-// ----- End main loop
 
 // --------- Helper Functions -------
 
-/*// -------- Motor Funcs --------*/
-/*void motorOn(){*/
-    /*digitalWrite(EnablePin, HIGH);*/
-    /*analogWrite(PWMPin2, 0);*/
-/*}*/
-/*void motorCW(int input_speed) {*/
-    /*PWMPin = 10;*/
-    /*PWMPin2 = 11;*/
-    /*motorWrite(input_speed);*/
-    /*motor_CW = true;*/
-/*}*/
-/*void motorCCW(int input_speed) {*/
-    /*PWMPin = 11;*/
-    /*PWMPin2 = 10;*/
-    /*motorWrite(input_speed);*/
-    /*motor_CW = false;*/
-/*}*/
-/*void motorWrite(int input_speed) {*/
-/*//    for(duty = 0; duty <= input_speed; duty += 50){*/
-/*//        analogWrite(PWMPin, duty);*/
-/*//        delay(5);*/
-/*//    }*/
-  /*analogWrite(PWMPin, input_speed);*/
-/*}*/
+// -------- Motor Funcs --------
+void motorOn(){
+    digitalWrite(EnablePin, HIGH);
+    analogWrite(PWMPin2, 0);
+// requires continuously writing it as fast as possible
+}
+void motorCW(int input_speed) {
+    PWMPin = 10;
+    PWMPin2 = 11;
+    motorWrite(input_speed);
+    motor_CW = true;
+}
+void motorCounterCW(int input_speed) {
+    PWMPin = 11;
+    PWMPin2 = 10;
+    motorWrite(input_speed);
+    motor_CW = false;
+}
+void motorWrite(int input_speed) {
+    for(duty = 0; duty <= input_speed; duty += 50){
+        analogWrite(PWMPin, duty);
+        delay(5);
+    }
+  analogWrite(PWMPin, input_speed);
+}
     
 // -------- Angle Conversion --------
 double getCurrentTheta1() { // calibration for stick encoder = 1250
@@ -238,7 +234,9 @@ ISR(PCINT1_vect) { // stick, on A5 and A4
     }
 }
 
-//---- print help
+
+
+//---- print help ---------
 int aprintf(char *str, ...) {
   int i, j, count = 0;
 
