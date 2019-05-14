142d25d3212ea5cdd5ecfd13524340c8bff8ab9e
akrishna23 <akrishna23@yahoo.com>
2019-05-14 01:00:14 -0400

    accidental swingup bangbang.ino

--- bangbang/bangbang.ino
+++ bangbang/bangbang.ino
@@ -1,3 +1,4 @@
+// this one accidentally swings up
 // bang bang controller 
 // 13 May 2019
 
@@ -130,11 +131,11 @@ void loop(){
 
     if (theta1 > 5) {
         if (theta1dot > 0.01) {
-            motorWrite(motor_output);
+            motorWrite(-motor_output);
             /*motorCCW(abs(motor_output));*/
         }
         else if (theta1dot < 0.01) {
-            motorWrite(-motor_output);
+            motorWrite(motor_output);
             /*motorCW(abs(motor_output));*/
         }
         else {
@@ -143,11 +144,11 @@ void loop(){
     }
     else if (theta1 < 5) {
         if (theta1dot > 0.01) {
-            motorWrite(motor_output);
+            motorWrite(-motor_output);
             /*motorCCW(abs(motor_output));*/
         }
         else if (theta1dot < -0.01) {
-            motorWrite(-motor_output);
+            motorWrite(motor_output);
             /*motorCW(abs(motor_output));*/
         }
         else {
@@ -156,8 +157,8 @@ void loop(){
     }
     else {
         // theta angle small; do nothing or use
-        //motor.Stop();
-        motorWrite(1);
+        motor.Stop();
+        /*motorWrite(1);*/
     }
 
     // SANITY CHECK
